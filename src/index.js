f508bb628c7895e472445177ddeb12cbf988cb1a
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-05-26 20:32:32 -0500

    completed exercise

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 "use strict";
 
+const $ = require('jquery');
+
 let sayHello = () => console.log("Hello");
 
 sayHello();
+console.log('test')
+console.log('numbertwo')
+
+
+$('body').css('background-color', 'blue');
+$('body').html(`<h1>Hello World</h1>`);
+$('body').append(`<h2>Aaron</h2>`);
+$('body').append(`<h3>Singleterry</h3>`)
